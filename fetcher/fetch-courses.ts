0e10375004e383dfb5b4046cf09a867f74cf349f
ninest <parth.kabra@gmail.com>
2022-07-23 22:41:26 +0800

    Add previous course merging

--- fetcher/fetch-courses.ts
+++ fetcher/fetch-courses.ts
@@ -7,8 +7,8 @@ import { readJSON, writeJSON } from "@/util/file.ts";
 import { deepmerge } from "deepmergets";
 import _ from "lodash";
 
-const subjects = await readJSON<Subject[]>(`${FOLDER_PATH}/subjects.json`);
-// const subjects = [{ code: "CS" }];
+// const subjects = await readJSON<Subject[]>(`${FOLDER_PATH}/subjects.json`);
+const subjects = [{ code: "CS" }];
 
 // Get the cookie
 const { cookie } = await getTerms({ noTerms: 1 /* Can it always be one? */ });
@@ -17,22 +17,31 @@ const noSubjects = subjects?.length;
 for await (const [subjectIndex, subject] of subjects!.entries()) {
   const courses: Course[] = [];
 
+  // Get previous courses to merge in with, so no need to refetch descriptions/others after fetching courses
+  const previouslyFetchedCourses = await readJSON<Course[]>(
+    `${FOLDER_PATH}/courses/${subject.code}.json`
+  );
+
   const noTerms = TERMS?.length;
   for await (const [termIndex, term] of TERMS!.entries()) {
     await searchPost(cookie, term.code);
-    const coursesForTerm = await getCoursesForTerm(
-      cookie,
-      term.code,
-      subject.code
-    );
+    const coursesForTerm = await getCoursesForTerm(cookie, term.code, subject.code);
 
-    courses.push(...coursesForTerm.map(transformCourse));
+    const transformedCourses = coursesForTerm.map(transformCourse);
 
-    console.log(
-      `${subjectIndex + 1}/${noSubjects} : ${
-        termIndex + 1
-      }/${noTerms} terms done`
-    );
+    // Merge and push transformedCourses to courses
+    transformedCourses.forEach((course) => {
+      const previousCourse = previouslyFetchedCourses?.find((c) => c.number == course.number);
+      if (previousCourse) {
+        courses.push(deepmerge(previousCourse, course));
+      } else {
+        // If it doesn't exist, push as normal
+        courses.push(course);
+      }
+    });
+    // courses.push(...coursesForTerm.map(transformCourse));
+
+    console.log(`${subjectIndex + 1}/${noSubjects} : ${termIndex + 1}/${noTerms} terms done`);
   }
 
   /* 
@@ -43,10 +52,7 @@ for await (const [subjectIndex, subject] of subjects!.entries()) {
   const courseNumberToCourses = _.groupBy(courses, "number");
 
   const uniqueCourses: Course[] = Object.keys(courseNumberToCourses)
-    .map(
-      (courseNumber) =>
-        deepmerge(...courseNumberToCourses[courseNumber]) as Course
-    )
+    .map((courseNumber) => deepmerge(...courseNumberToCourses[courseNumber]) as Course)
     // Extra step: remove duplicates in NUpath list
     .map((course: Course) => ({ ...course, nuPath: _.uniq(course.nuPath) }));
 
