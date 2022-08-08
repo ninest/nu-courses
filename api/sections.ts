d558c955afbfa98459417736507b9bcaf463df2d
ninest <parth.kabra@gmail.com>
2022-08-08 14:33:51 +0800

    Add error retry for sections API

--- api/sections.ts
+++ api/sections.ts
@@ -1,14 +1,33 @@
 import { Router } from "oak";
+import { getFacultyMeetTimes } from "@/banner/faculty-meet-times.ts";
+import { getCourseSeats } from "@/banner/seats.ts";
+import { transformFacultyMeetTime } from "@/transformers/faculty-meet-times.ts";
+import { transformSeats } from "@/transformers/seats.ts";
+import { FacultyMeetingTime, Seats, SectionInfo } from "@/types.ts";
 
 export const sectionsRouter = new Router();
 
-interface Params {
-  term: string;
-  crn: string;
-}
-
-sectionsRouter.get("/:term/:crn", (context) => {
-  const { term, crn } = context.params as Params;
-
-  context.response.body = { term, crn };
+sectionsRouter.get("/:term/:crn", async (context) => {
+  const section = context.params as SectionInfo;
+  while (true) {
+    try {
+      const sectionData = await getSectionData(section);
+      context.response.body = sectionData;
+      break;
+    } catch (error) {
+      // TODO: throw error if not response in 5 tries
+    }
+  }
 });
+
+const getSectionData = async (
+  section: SectionInfo
+): Promise<{ facultyMeetTime: FacultyMeetingTime; seats: Seats }> => {
+  const [facultyMeetTimesJson, seatsHtml] = await Promise.all([
+    getFacultyMeetTimes(section),
+    getCourseSeats(section),
+  ]);
+  const facultyMeetTime = facultyMeetTimesJson.map(transformFacultyMeetTime)[0];
+  const seats = transformSeats(seatsHtml);
+  return { facultyMeetTime, seats };
+};
