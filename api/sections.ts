8da58908150d26a2be1b974bf4228b364002e9f1
ninest <34677361+ninest@users.noreply.github.com>
2022-08-09 11:32:26 +0800

    Sections API (#15)

---
+++ api/sections.ts
@@ -0,0 +1,33 @@
+import { Router } from "oak";
+import { getFacultyMeetTimes } from "@/banner/faculty-meet-times.ts";
+import { getCourseSeats } from "@/banner/seats.ts";
+import { transformFacultyMeetTime } from "@/transformers/faculty-meet-times.ts";
+import { transformSeats } from "@/transformers/seats.ts";
+import { FacultyMeetingTime, Seats, SectionInfo } from "@/types.ts";
+
+export const sectionsRouter = new Router();
+
+sectionsRouter.get("/:term/:crn", async (context) => {
+  const section = context.params as SectionInfo;
+  while (true) {
+    try {
+      const sectionData = await getSectionData(section);
+      context.response.body = sectionData;
+      break;
+    } catch (error) {
+      // TODO: throw error if no response in 5 tries
+    }
+  }
+});
+
+const getSectionData = async (
+  section: SectionInfo
+): Promise<{ facultyMeetTime: FacultyMeetingTime; seats: Seats }> => {
+  const [facultyMeetTimesJson, seatsHtml] = await Promise.all([
+    getFacultyMeetTimes(section),
+    getCourseSeats(section),
+  ]);
+  const facultyMeetTime = facultyMeetTimesJson.map(transformFacultyMeetTime)[0];
+  const seats = transformSeats(seatsHtml);
+  return { facultyMeetTime, seats };
+};
