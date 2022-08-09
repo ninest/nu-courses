3c808eff397f235da1ffe09b5e267cf29d4b6f3e
ninest <parth.kabra@gmail.com>
2022-08-09 13:10:17 +0800

    Add CORS

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,11 @@
 import { Application, Router } from "oak";
+import { oakCors } from "cors";
 import { sectionsRouter } from "@/api/sections.ts";
 
 const app = new Application();
 const sections = new Router().use("/sections", sectionsRouter.routes());
+
+app.use(oakCors());
 app.use(sections.routes(), sections.allowedMethods());
 
 console.log("Listening on http://localhost:3000");
