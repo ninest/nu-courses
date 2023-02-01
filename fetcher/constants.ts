2d08de9b90f441e6fa6257a40f791eab6b040503
ninest <parth.kabra@gmail.com>
2023-01-31 23:56:00 -0500

    Fetch summer courses

--- fetcher/constants.ts
+++ fetcher/constants.ts
@@ -2,16 +2,28 @@ export const FOLDER_PATH = "./.data";
 
 export const TERMS = [
   {
+    code: "202360",
+    description: "Summer II 2023",
+  },
+  {
+    code: "202340",
+    description: "Summer I 2023",
+  },
+  {
+    code: "202350",
+    description: "Summer Full 2023",
+  },
+  {
     code: "202330",
-    description: "Spring 2023 Semester",
+    description: "Spring 2023",
   },
   {
     code: "202310",
-    description: "Fall 2022 Semester",
+    description: "Fall 2022",
   },
   {
     code: "202260",
-    description: "Summer 2 2022 Semester",
+    description: "Summer 2 2022",
   },
   {
     code: "202240",
