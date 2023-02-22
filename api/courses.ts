e7ddaf9fe8c3dd33fca71ceecfa3b318c8c3d1f2
ninest <parth.kabra@gmail.com>
2023-02-22 11:44:20 -0500

    Add single course API

--- api/courses.ts
+++ api/courses.ts
@@ -23,3 +23,30 @@ coursesRouter.get("/:subjectCode", async (context) => {
   context.response.status = 200;
   context.response.body = { data: courses };
 });
+
+coursesRouter.get("/:subjectCode/:courseNumber", async (context) => {
+  const { subjectCode, courseNumber } = context.params;
+  const subjects = await readJSON<Subject[]>(`${DATA_DIR_PATH}/subjects.json`);
+
+  const subject = subjects?.find((s) => s.code === subjectCode);
+  if (!subject) {
+    context.response.status = 400;
+    context.response.body = {
+      message: "Invalid subject code",
+    };
+    return;
+  }
+
+  const courses = await readJSON<Course[]>(`${DATA_DIR_PATH}/courses/${subjectCode}.json`);
+  const course = courses?.find((c) => c.subject === subjectCode && c.number === courseNumber);
+  if (!subject) {
+    context.response.status = 400;
+    context.response.body = {
+      message: "Invalid course number or course",
+    };
+    return;
+  }
+
+  context.response.status = 200;
+  context.response.body = { data: course };
+});
