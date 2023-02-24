f4da3fac02a3b7c00c8692693e660325abf439d6
ninest <parth.kabra@gmail.com>
2023-02-23 20:50:33 -0500

    Migrate sections API to hono

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,9 @@
-import { Application, Router } from "oak";
-import { oakCors } from "cors";
-import { sectionsRouter } from "@/api/sections.ts";
-import { termCoursesRouter } from "./term-courses.ts";
-import { coursesRouter } from "./courses.ts";
-import { subjectsRouter } from "./subjects.ts";
+import { Hono } from "hono";
+import { serve } from "http-server";
 
-const app = new Application();
-const router = new Router()
-  .use("/subjects", subjectsRouter.routes())
-  .use("/courses", coursesRouter.routes())
-  .use("/sections", sectionsRouter.routes())
-  .use("/mapping/term-courses", termCoursesRouter.routes());
+import { sectionsRouter } from "./sections.ts";
 
-app.use(oakCors());
-app.use(router.routes(), router.allowedMethods());
+const app = new Hono();
+app.route("/sections", sectionsRouter);
 
-console.log("Listening on http://localhost:3000");
-await app.listen({ port: 3000 });
+serve(app.fetch, { port: 3000 });
--- api/old/sections.ts
+++ api/old/sections.ts
@@ -1,32 +1,37 @@
+import { Router } from "oak";
 import { getFacultyMeetTimes } from "@/banner/faculty-meet-times.ts";
 import { getCourseSeats } from "@/banner/seats.ts";
 import { transformFacultyMeetTime } from "@/transformers/faculty-meet-times.ts";
 import { transformSeats } from "@/transformers/seats.ts";
-import { Hono } from "hono";
-import { SectionInfo } from "../types.ts";
+import { FacultyMeetingTime, Seats, SectionInfo } from "@/types.ts";
 import { SectionsResponse } from "./types.ts";
 
-export const sectionsRouter = new Hono();
+export const sectionsRouter = new Router();
 
-sectionsRouter.get("/:term/:crn", async (c) => {
-  const section: SectionInfo = {
-    term: c.req.param("term"),
-    crn: c.req.param("crn"),
-  };
-
+sectionsRouter.get("/:term/:crn", async (context) => {
+  const section = context.params as SectionInfo;
   let retries = 0;
   while (true) {
     try {
       const sectionData = await getSectionData(section);
-      return c.json(sectionData);
+      context.response.body = sectionData;
+      break;
     } catch (error) {
       retries += 1;
-      if (retries >= 3) return c.json({ message: "Unable to find section information" }, 400);
+      if (retries >= 3) {
+        context.response.status = 400;
+        context.response.body = {
+          message: "Unable to find section information",
+        };
+        return;
+      }
     }
   }
 });
 
-const getSectionData = async (section: SectionInfo): Promise<SectionsResponse> => {
+const getSectionData = async (
+  section: SectionInfo,
+): Promise<SectionsResponse> => {
   const [facultyMeetTimesJson, seatsHtml] = await Promise.all([
     getFacultyMeetTimes(section),
     getCourseSeats(section),
